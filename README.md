33e97d1809978eb83ba09a68a8491a59e70eeae2
Michel Palourdio <mpalourdio@gmail.com>
2018-12-16 15:10:44 +0100

    Migrate from zuul to Spring Cloud Gateway

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 [![Build Status](https://travis-ci.org/mpalourdio/ProxyServer.svg?branch=master)](https://travis-ci.org/mpalourdio/ProxyServer)
 
-Spring Cloud Proxy Server (zuul)
-================================
+Spring Cloud Gateway Server
+===========================
--- src/main/java/com/mpalourdio/proxyserver/ProxyServerApplication.java
+++ src/main/java/com/mpalourdio/proxyserver/ProxyServerApplication.java
@@ -12,10 +12,8 @@ package com.mpalourdio.proxyserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 @EnableDiscoveryClient
 public class ProxyServerApplication {
 
