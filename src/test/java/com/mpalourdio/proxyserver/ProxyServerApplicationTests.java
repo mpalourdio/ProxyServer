4aa97622d80d7db6c777d30ec4fb8fedc613747e
Michel Palourdio <mpalourdio@gmail.com>
2019-03-16 08:02:29 +0100

    Upgrade to JUnit 5

--- src/test/java/com/mpalourdio/proxyserver/ProxyServerApplicationTests.java
+++ src/test/java/com/mpalourdio/proxyserver/ProxyServerApplicationTests.java
@@ -9,13 +9,10 @@
 
 package com.mpalourdio.proxyserver;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(locations = "classpath:test.properties")
 public class ProxyServerApplicationTests {
