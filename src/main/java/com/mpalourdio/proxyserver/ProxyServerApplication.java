bed48dc5f5e7a46f89dc898fc3a94a66db744487
Michel Palourdio <mpalourdio@gmail.com>
2025-11-01 14:56:13 +0100

    chore: polish SB4 migration

--- src/main/java/com/mpalourdio/proxyserver/ProxyServerApplication.java
+++ src/main/java/com/mpalourdio/proxyserver/ProxyServerApplication.java
@@ -15,9 +15,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProxyServerApplication {
+class ProxyServerApplication {
 
-    public static void main(String... args) {
+    static void main(String... args) {
         SpringApplication.run(ProxyServerApplication.class, args);
     }
 }
