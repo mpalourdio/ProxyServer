65d20dbb0acc61f026bc01f41c71b1aa10aa5d9a
Michel Palourdio <mpalourdio@gmail.com>
2026-06-12 15:55:44 +0200

    chore: Spring Cloud 2025.1.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ repositories {
     mavenCentral()
 }
 
-extra["springCloudVersion"] = "2025.1.1"
+extra["springCloudVersion"] = "2025.1.2"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
